36f59bc2a5d034e0b4d970c0a4354910ad293caa
shubhammodi004 <shubhammodi004@example.com>
2019-05-12 13:48:43 +0530

    Loader done

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,64 @@
+import React, {Component} from 'react'
+import "./Loader.css"
+import Img from "gatsby-image";
+import { StaticQuery, graphql, Link } from "gatsby"
+import Fade from 'react-reveal/Fade';
+
+class Loader extends Component {
+
+    state = {
+        show: true,
+        showScreen: true
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                show: false,
+            })
+        }, 600);
+        setTimeout(() => {
+            this.setState({
+                showScreen: false,
+            })
+        }, 1200);
+    }
+
+    render() {
+        const {show,showScreen} = this.state;
+        return (
+            <div>
+                <Fade top when={showScreen} duration={1500}>
+                    <div className="loader">
+                        <Fade top when={show} duration={800}>
+                            <div className="loaderLogo">
+                                <Logo />
+                            </div>
+                        </Fade>
+                    </div>
+                </Fade>
+            </div>
+        )
+    }
+}
+
+const Logo = () => (
+    <StaticQuery
+        query={graphql`
+      query {
+        placeholderImage: file(relativePath: { eq: "logo.png" }) {
+          childImageSharp {
+            fluid{
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    `}
+        render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+    />
+)
+
+export default Loader;
+
+
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,16 @@ import { StaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Header from "../components/header"
+import Loader from "../components/Loader/Loader"
+
 import Hero from "../components/HomePage/Hero"
 import About from "../components/HomePage/About"
 import Work from "../components/HomePage/Work/Work"
 import Myprocess from "../components/HomePage/Myprocess/Myprocess"
 import Blog from "../components/HomePage/Blog/Blog"
+
+// For the fade in effect;
+import Fade from 'react-reveal/Fade';
 
 import SEO from "../components/seo"
 
@@ -24,21 +29,23 @@ class IndexPage extends Component {
 			this.setState({
 				render: true
 			})
-		}, 4000);
+		}, 1800);
 	}
 
 	render() {
 		const {render } = this.state;
 		return (
-			(!render ? <Header/> :
-				<Layout>
-					<Header />
-					<Hero />
-					<About />
-					<Work />
-					<Myprocess />
-					<Blog />
-				</Layout>
+			(!render ? <Loader/> :
+				<Fade delay={1200} duration={400}>
+					<Layout>
+						<Header />
+						<Hero />
+						<About />
+						<Work />
+						<Myprocess />
+						<Blog />
+					</Layout>
+				</Fade>
 			)
 		)
 	}
